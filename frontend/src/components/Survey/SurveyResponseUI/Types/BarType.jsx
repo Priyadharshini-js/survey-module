f6f042d53f402cc099f86283c820baae1ea4894c
Priyadharshini Kannan <pkannan@AD.MSYSTECHNOLOGIES.COM>
2025-07-11 13:04:46 +0530

    label added for barchart.

--- frontend/src/components/Survey/SurveyResponseUI/Types/BarType.jsx
+++ frontend/src/components/Survey/SurveyResponseUI/Types/BarType.jsx
@@ -7,16 +7,22 @@ const BarType = ({ value }) => {
     return (
         <>
             <div>
-                <div className="d-flex gap-1">
-                    {Array.from({ length: totalSegments }, (_, index) => {
-                        const isFilled = index < value;
-                        return (
-                            <div
-                                key={index}
-                                className={`progress-segment ${isFilled ? 'bg-secondary' : 'grey'}`}
-                            ></div>
-                        )
-                    })}
+                <div className="bar-type-wrapper">
+                    <div className="d-flex gap-1 mb-2">
+                        {Array.from({ length: totalSegments }, (_, index) => {
+                            const isFilled = index < value;
+                            return (
+                                <div
+                                    key={index}
+                                    className={`progress-segment ${isFilled ? 'bg-secondary' : 'grey'}`}
+                                ></div>
+                            )
+                        })}
+                    </div>
+                    <div className="d-flex justify-content-between mb-1">
+                        <span className="text-muted small">Extremely Unsatisfied</span>
+                        <span className="text-muted small">Extremely Satisfied</span>
+                    </div>
                 </div>
             </div>
         </>
